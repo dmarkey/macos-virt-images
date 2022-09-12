1cf09639db3de26fc5dc67578d78a55106897a2e
David Markey <dmarkey@rriadmins-MBP.lan>
2022-09-12 16:10:38 +0100

    More tweaks

--- builders/debian/build.sh
+++ builders/debian/build.sh
@@ -52,16 +52,15 @@ echo fuse >> /etc/modules
 systemctl enable getty@hvc0
 systemctl enable ssh
 systemctl enable macos-virt-service
-systemctl enable networking
-echo "auto enp0s1" >> /etc/network/interfaces.d/enp0s1.conf
-echo "allow-hotplug enp0s1" >> /etc/network/interfaces.d/enp0s1.conf
-echo "iface enp0s1 inet dhcp" >> /etc/network/interfaces.d/enp0s1.conf
-echo "auto enp0s0" >> /etc/network/interfaces.d/enp0s0.conf
-echo "allow-hotplug enp0s0" >> /etc/network/interfaces.d/enp0s0.conf
-echo "iface enp0s0 inet dhcp" >> /etc/network/interfaces.d/enp0s0.conf
+systemctl enable systemd-networkd
+echo "[Match]" > /etc/systemd/network/20-wired.network"
+echo "Name=enp0*" >> /etc/systemd/network/20-wired.network"
+echo "[Network]" >> /etc/systemd/network/20-wired.network"
+echo "DHCP=yes" >> /etc/systemd/network/20-wired.network"
 echo 'root:password' | chpasswd
 echo "/dev/vda      /    ext4   defaults        0 0" >> /etc/fstab
 echo "/dev/vdb      /boot    udf   defaults        0 0" >> /etc/fstab
+echo localhost > /etc/hostname
 EOF
 
 chroot "$ROOT" /bin/sh /tmp/init.sh
