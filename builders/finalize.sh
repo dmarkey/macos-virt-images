794d395ae1554dade436a8d70dd0fcacc5408198
David Markey <dmarkey@rriadmins-MBP.lan>
2022-09-12 12:22:36 +0100

    Cleanup

--- builders/finalize.sh
+++ builders/finalize.sh
@@ -9,3 +9,4 @@ cp $BOOT_CONFIG /working/$NAME/$RELEASE/$ARCH/boot/boot.cfg
 umount /working/$NAME/$RELEASE/$ARCH/boot/ /working/$NAME/$RELEASE/$ARCH
 cd /images/$NAME/$RELEASE/$ARCH/
 tar zcvf package.tar.gz root.img boot.img
+rm root.img boot.img
