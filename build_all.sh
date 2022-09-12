09e7de9701a26fe5d0b226196c1376e4cb1dc174
David Markey <dmarkey@Davids-MacBook-Pro.local>
2022-09-12 11:50:26 +0100

    Parallellize

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-bash build.sh rocky rocky 8.5 none rocky.cfg "Rocky Linux 8.5"
-bash build.sh alpine alpine 3.16 none alpine.cfg "Alpine 3.16"
-bash build.sh alpine alpine 3.15 none alpine.cfg "Alpine 3.15"
-bash build.sh alpine alpine 3.14 none alpine.cfg "Alpine 3.14"
-bash build.sh debian ubuntu focal linux-aws ubuntu.cfg "Ubuntu 20.04(Focal)"
-bash build.sh debian ubuntu jammy linux-aws ubuntu.cfg "Ubuntu 22.04(Jammy)"
-bash build.sh debian debian bullseye sshfs  debian.cfg "Debian 11(Bullseye)"
+bash build.sh rocky rocky 8.5 none rocky.cfg "Rocky Linux 8.5" &
+bash build.sh alpine alpine 3.16 none alpine.cfg "Alpine 3.16" &
+bash build.sh alpine alpine 3.15 none alpine.cfg "Alpine 3.15" &
+bash build.sh alpine alpine 3.14 none alpine.cfg "Alpine 3.14" &
+bash build.sh debian ubuntu focal linux-aws ubuntu.cfg "Ubuntu 20.04(Focal)" &
+bash build.sh debian ubuntu jammy linux-aws ubuntu.cfg "Ubuntu 22.04(Jammy)" &
+bash build.sh debian debian bullseye sshfs  debian.cfg "Debian 11(Bullseye)" &
+
+wait
