eb2112bf53c7d3285f1fad88312afd34a6b6373d
David Markey <dmarkey@Davids-MacBook-Pro.local>
2022-09-26 00:11:40 +0100

    Fedora tweaks

--- builders/fedora/init_fedora.sh
+++ builders/fedora/init_fedora.sh
@@ -14,7 +14,8 @@ echo "Name=enp0*" >> /etc/systemd/network/20-wired.network
 echo "[Network]" >> /etc/systemd/network/20-wired.network
 echo "DHCP=yes" >> /etc/systemd/network/20-wired.network
 echo 'root:password' | chpasswd
-echo "/dev/vda      /    ext4   defaults        0 0" >> /etc/fstab
+echo "/dev/vda      /    ext4   defaults        0 0" > /etc/fstab
+echo "/dev/vdb      /boot    vfat   defaults        0 0" >> /etc/fstab
 echo 'LANG="en_US.UTF-8"' >> /etc/environment
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/environment
 echo 'LC_CTYPE="en_US.UTF-8"' >> /etc/environment
