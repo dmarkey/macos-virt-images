5fea9cc58c7d3c1dfba50d74f91cf0b3da3a523c
David Markey <dmarkey@rriadmins-MacBook-Pro.local>
2022-09-26 10:30:18 +0100

    Tweak

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ tar -xvf $$full_rootfs.tar  boot -C $$boot
 find $$boot -type l -delete
 bsdtar -c -L -f $$rootfs.tar -p --exclude='boot/*' @$$full_rootfs.tar
 tar -rvf  $$rootfs.tar -C ./service/ etc/resolv.conf etc/hosts etc/hostname --owner=0 --group=0
-virt-make-fs --type=ext4 -s 1G $$rootfs.tar root.img
-virt-make-fs --type=vfat -s 100M ./$$boot/ boot.img
+virt-make-fs --type=ext4 -s 1G $$rootfs.tar $$root.img
+virt-make-fs --type=vfat -s 100M ./$$boot/ $$boot.img
 tar -cvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar $$root.img $$boot.img
 tar -rvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar  -C builders/"${NAME}" boot.cfg
 gzip -f output/"$NAME"-"$VERSION"-"$(uname -m)".tar
