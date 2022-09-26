26aa635b794467ec1a77d8733a3d1e201b01d8e5
David Markey <dmarkey@rriadmins-MacBook-Pro.local>
2022-09-26 10:31:19 +0100

    Fail fast

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 sudo apt-get install -y libguestfs-tools libarchive-tools
 mkdir -p output || true
-sudo bash build.sh alpine 3.14
+sudo build.sh alpine 3.14
 #sudo bash build.sh alpine 3.15
 #sudo bash build.sh alpine 3.16
