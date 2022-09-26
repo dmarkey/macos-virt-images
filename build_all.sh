8b7f89de507e1d629a609e6ec7323475ec002c6b
David Markey <dmarkey@rriadmins-MacBook-Pro.local>
2022-09-26 10:02:43 +0100

    Try this

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-bash build.sh alpine alpine 3.16 none alpine.cfg "Alpine 3.16" &
-bash build.sh alpine alpine 3.15 none alpine.cfg "Alpine 3.15" &
-bash build.sh alpine alpine 3.14 none alpine.cfg "Alpine 3.14" &
-bash build.sh debian ubuntu focal linux-aws ubuntu.cfg "Ubuntu 20.04(Focal)" &
-bash build.sh debian ubuntu jammy linux-aws ubuntu.cfg "Ubuntu 22.04(Jammy)" &
-bash build.sh debian debian bullseye sshfs  debian.cfg "Debian 11(Bullseye)" &
-
-wait
+apt-get install -y libguestfs-tools libarchive-tools
+mkdir -p output || true
+bash build.sh alpine 3.16
