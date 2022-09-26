1fb555e1ec0c3f4cdc64be1deb73d316f781143a
David Markey <dmarkey@rriadmins-MacBook-Pro.local>
2022-09-26 10:26:24 +0100

    Try this

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 NAME=$1
 VERSION=$2
 
-docker buildx build --output type=tar --build-arg VERSION="${VERSION}" . -f builders/"${NAME}"/Dockerfile > full_rootfs.tar
+docker buildx build --output type=tar --build-arg VERSION="${VERSION}" . -f builders/"${NAME}"/Dockerfile > $$full_rootfs.tar
 rm -rf boot || true
-mkdir boot
-tar -xvf full_rootfs.tar  boot -C boot
-find boot -type l -delete
-bsdtar -c -L -f rootfs.tar -p --exclude='boot/*' @full_rootfs.tar
-tar -rvf  rootfs.tar -C ./service/ etc/resolv.conf etc/hosts etc/hostname --owner=0 --group=0
-virt-make-fs --type=ext4 -s 1G rootfs.tar root.img
-virt-make-fs --type=vfat -s 100M ./boot/ boot.img
-tar -cvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar root.img boot.img
+mkdir $$boot
+tar -xvf $$full_rootfs.tar  boot -C $$boot
+find $$boot -type l -delete
+bsdtar -c -L -f $$rootfs.tar -p --exclude='boot/*' @$$full_rootfs.tar
+tar -rvf  $$rootfs.tar -C ./service/ etc/resolv.conf etc/hosts etc/hostname --owner=0 --group=0
+virt-make-fs --type=ext4 -s 1G $$rootfs.tar root.img
+virt-make-fs --type=vfat -s 100M ./$$boot/ boot.img
+tar -cvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar $$root.img $$boot.img
 tar -rvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar  -C builders/"${NAME}" boot.cfg
 gzip -f output/"$NAME"-"$VERSION"-"$(uname -m)".tar
-rm -rf boot full_rootfs.tar boot.img root.img
+rm -rf $$boot $$full_rootfs.tar $$boot.img $$root.img
--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 sudo apt-get install -y libguestfs-tools libarchive-tools
 mkdir -p output || true
-sudo bash build.sh alpine 3.16
+sudo bash build.sh alpine 3.14
+#sudo bash build.sh alpine 3.15
+#sudo bash build.sh alpine 3.16
