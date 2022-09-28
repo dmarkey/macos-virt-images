d9daa243784ab324f2d57791c378b44e58b132f8
David Markey <dmarkey@Davids-MacBook-Pro.local>
2022-09-28 21:35:13 +0100

    Change directory here

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ bsdtar -c -L -f "$WORKDIR"/rootfs.tar -p --exclude='boot/*' @"$WORKDIR"/full_roo
 tar -rvf "$WORKDIR"/rootfs.tar -C ./service/ etc/resolv.conf etc/hosts etc/hostname --owner=0 --group=0
 virt-make-fs --type=ext4 -s 1G  "$WORKDIR"/rootfs.tar  "$WORKDIR"/root.img
 virt-make-fs --type=vfat -s 100M  "$WORKDIR"/boot/  "$WORKDIR"/boot.img
-tar -cvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar "$WORKDIR"/root.img "$WORKDIR"/boot.img
-tar -rvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar  -C builders/"${NAME}" boot.cfg
+tar -cvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar -C "$WORKDIR"  "$WORKDIR"/root.img "$WORKDIR"/boot.img
+tar -rvf output/"$NAME"-"$VERSION"-"$(uname -m)".tar -C builders/"${NAME}" boot.cfg
 gzip -f output/"$NAME"-"$VERSION"-"$(uname -m)".tar
 rm -rf "$WORKDIR"
